de0321b497631c9f869fbd8e9b2b7fe7c0d82950
Nikita Satish Madke <madkenikita35@gmail.com>
2026-04-15 21:00:48 +0530

    Add Code

--- index.js
+++ index.js
@@ -1,15 +1,27 @@
 import express from "express";
 import mongoose from "mongoose";
-import dotevn from "dotenv";
+import dotenv from "dotenv";
 
+dotenv.config();
 const app = express();
-dotevn.config();
 
 app.use(express.json());
 
 const PORT = process.env.PORT || 5000;
 const uri = process.env.MONGO_URI;
 
+// const PORT = 5000;
+// const uri =
+//   "mongodb+srv://madkenikita35_db_user:Nikita@url.6ufirbp.mongodb.net/?appName=URL";
+
 mongoose.connect(uri).then(() => {
   console.log("Mongodb Connected");
+});
+
+app.get("/", (req, res) => {
+  res.send("Home Page");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is Running on the Port ${PORT}`);
 });
